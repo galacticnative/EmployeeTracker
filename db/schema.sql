562d822aaf170ce1910ecfee98c6447ea16b2883
galacticnative <68270151+galacticnative@users.noreply.github.com>
2020-10-14 15:42:40 -0700

    created scripts

---
+++ db/schema.sql
@@ -0,0 +1,19 @@
+CREATE TABLE employee (
+  id INTEGER PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT NULL
+);
+
+CREATE TABLE department (
+	id INTEGER PRIMARY KEY,
+	name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+	id INTEGER PRIMARY KEY,
+	title VARCHAR(30) NOT NULL,
+	salary DECIMAL,
+	department_id INT NOT NULL
+);
---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES 
+    ('Posh', 'Spice', 1, 3), 
+    ('Mel', 'Brown', 2, 4), 
+    ('Emma', 'Bunton', 3, 5), 
+    ('Melanie', 'Chisolm', 4, 3), 
+    ('Geri', 'Haliwell', 5, 4);
