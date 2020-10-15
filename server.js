df13d93f1af077e92f908508dadfb0628057d759
galacticnative <68270151+galacticnative@users.noreply.github.com>
2020-10-14 17:32:26 -0700

    create get and post routes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const sqlite3 = require('sqlite3').verbose();
+const inputCheck = require('./utils/inputCheck');
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
@@ -15,8 +16,86 @@ const db = new sqlite3.Database('./db/business.db', err => {
     console.log('Connected to the BUSINESS database.');
 });
 
-db.all(`SELECT * FROM employee`, (err, rows) => {
-    console.log(rows);
+// db.all(`SELECT * FROM employee`, (err, rows) => {
+//     console.log(rows);
+// });
+
+// Get single employee
+app.get('/api/employee/:id', (req, res) => {
+    const sql = `SELECT * FROM employee 
+                 WHERE id = ?`;
+    const params = [req.params.id];
+    db.get(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+  
+      res.json({
+        message: 'success',
+        data: row
+      });
+    });
+});
+
+// Delete an employee
+app.delete('/api/employee/:id', (req, res) => {
+    const sql = `DELETE FROM employee WHERE id = ?`;
+    const params = [req.params.id];
+    db.run(sql, params, function(err, result) {
+      if (err) {
+        res.status(400).json({ error: res.message });
+        return;
+      }
+  
+      res.json({
+        message: 'successfully deleted',
+        changes: this.changes
+      });
+    });
+});
+
+// Create an employee POST
+app.post('/api/employee', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'role_id', 'manager_id');
+    if (errors) {
+      res.status(400).json({ error: errors });
+      return;
+    }
+    const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id) 
+              VALUES (?,?,?,?)`;
+    const params = [body.first_name, body.last_name, body.role_id, body.manager_id];
+    // ES5 function, not arrow function, to use `this`
+    db.run(sql, params, function(err, result) {
+    if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+    }
+
+    res.json({
+        message: 'success',
+        data: body,
+        id: this.lastID
+    });
+    });
+});
+
+
+// Get all employees
+app.get('/api/employee', (req, res) => {
+    const sql = `SELECT * FROM employee`;
+    const params = [];
+    db.all(sql, params, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+  
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
 });
 
 // when user enter invalid request, must be LAST response; CATCH ALL
