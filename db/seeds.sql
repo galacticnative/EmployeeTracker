0c987a39133499dd242cc09269dae1962d668436
galacticnative <68270151+galacticnative@users.noreply.github.com>
2020-10-15 11:54:34 -0700

    added department and role tables

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,3 +5,23 @@ VALUES
     ('Emma', 'Bunton', 3, 5), 
     ('Melanie', 'Chisolm', 4, 3), 
     ('Geri', 'Haliwell', 5, 4);
+
+INSERT INTO department (name)
+VALUES 
+    ('Engineering'),
+    ('Finance'),
+    ('Legal'),
+    ('Sales');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+    ('Lead Engineer', 150000, 1),
+    ('Software Engineer', 120000, 1),
+    ('Accountant', 125000, 2),
+    ('Lead Legal Counsel', 250000, 3),
+    ('Lawyer', 190000, 3),
+    ('Sales Lead', 100000, 4),
+    ('Salesperson', 80000, 4);
+
+
+    
