0381520ebd15bf44dfa940f3e39361495b09c7b5
galacticnative <68270151+galacticnative@users.noreply.github.com>
2020-10-18 16:48:51 -0700

    update view roles

--- db/seeds.sql
+++ db/seeds.sql
@@ -13,15 +13,15 @@ VALUES
     ('Lead Legal Counsel', 250000, 3),
     ('Lawyer', 190000, 3),
     ('Sales Lead', 100000, 4),
-    ('Salesperson', 80000, 4);
+    ('Salesperson', 80000, 4); 
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
+INSERT INTO employee (first_name, last_name, role_id, manager)
 VALUES 
     ('Posh', 'Spice', 1, NULL), 
-    ('Mel', 'Brown', 2, 1), 
+    ('Mel', 'Brown', 2, 'Posh Spice'), 
     ('Emma', 'Bunton', 3, NULL), 
-    ('Melanie', 'Chisolm', 7, 6), 
+    ('Melanie', 'Chisolm', 7, 'Geri Haliwell'), 
     ('Geri', 'Haliwell', 6, NULL),
-    ('Britney', 'Spears', 5, 4),
+    ('Britney', 'Spears', 5, 'Fleetwood Mac'),
     ('Fleetwood', 'Mac', 4, NULL);
     
--- index.js
+++ index.js
@@ -15,28 +15,37 @@ const promptQuestions = () => {
             message: 'Select an option',
             choices: 
             ['View ALL Employees', 
-            'View by Department', 
+            'View Departments',
+            'View Roles', 
             'View by Manager', 
-            'Add Employee', 
-            'Remove Employee', 
+            'Add Employee',
+            'Add Department',
+            'Add Role', 
             'Update Role', 
-            'Update Manager']
+            ]
         }
     ])
     .then(choice => {
         if (choice.viewAll === 'View ALL Employees') {
             readEmployees();
-        } else if (choice.viewAll === 'View by Department') {
+        } else if (choice.viewAll === 'View Departments') {
             viewDepartment();
         } else if (choice.viewAll === 'View by Manager') {
             viewManager();
+        } 
+        else if (choice.viewAll === 'View Roles') {
+            viewRoles();
+        }
+        else if (choice.viewAll === 'Update Role') {
+            updateRole();
         }
     })
 
         function readEmployees() {
             console.log('Viewing all Employees');
             //connection.query(`SELECT * FROM employee`,
-            connection.query(`SELECT employee.*, 
+            connection.query(`
+            SELECT employee.*, 
             role.title AS title, role.salary AS salary
             FROM employee
             LEFT JOIN role ON employee.role_id = role.id`,
@@ -51,11 +60,8 @@ const promptQuestions = () => {
         function viewDepartment() {
             console.log('viewing by Department');
             connection.query(
-                `SELECT employee.*, role.department_id 
-                AS department_id 
-                FROM employee 
-                LEFT JOIN role 
-                ON employee.role_id = role.id`,
+                `SELECT id, name
+                FROM department`,
                 function(err, res) {
                     if (err) throw err;
                     console.table(res);
@@ -66,7 +72,7 @@ const promptQuestions = () => {
         function viewManager() {
             console.log('viewing managers');
             connection.query(
-                `SELECT first_name, last_name, manager_id
+                `SELECT first_name, last_name, manager
                 FROM employee`,
                 function(err, res) {
                     if(err) throw err;
@@ -75,15 +81,75 @@ const promptQuestions = () => {
                 });
         };
 
-        // function updateRole() {
-        //     console.log('updating role');
-        //     const query = connection.query(`UPDATE employee SET role_id = ? 
-        //     WHERE id = ?`, [], 
-        //     function(err, res) {
-        //         if (err) throw err;
-        //         console.log(res.affectedRows + ' role updated!!\n')
-        //     }
-        // );
+        function viewRoles() {
+            console.log('Viewing all ROLES');
+            connection.query(
+                `SELECT 
+                role.id AS ID,
+                role.title AS title,
+                role.salary AS $ALARY,
+                department.name AS department
+                FROM role
+                LEFT JOIN department ON role.department_id = department.id`,
+                function(err, res) {
+                    if(err) throw err;
+                    console.table(res);
+                    promptQuestions();
+                });
+        };
+
+        function addDepartment() {
+            return inquirer.prompt([
+        
+                {
+                    type: 'input',
+                    name: 'departmentname',
+                    message: 'Provide a new DEPARTMENT',
+                    validate: departmentInput => {
+                      if (departmentInput) {
+                        return true;
+                      } else {
+                        console.log('Please enter a Department name!');
+                        return false;
+                      }
+                    }
+                },
+            ])
+            .then()
+        };
+
+        function updateRole() {
+            return inquirer.prompt([
+        
+                {
+                    type: 'input',
+                    name: 'departmentname',
+                    message: 'Provide a new DEPARTMENT',
+                    validate: departmentInput => {
+                      if (departmentInput) {
+                        return true;
+                      } else {
+                        console.log('Please enter a Department name!');
+                        return false;
+                      }
+                    }
+                },
+            ])
+            .then(storeRole => {
+                if(storeRole === departmentInput) {
+
+                }
+            })
+
+            console.log('updating role');
+            const sql = `UPDATE department SET name = ? WHERE id = ?`;
+            const params = [req.body.name, req.params.id];
+            connection.query( 
+            function(err, res) {
+                if (err) throw err;
+                console.data(res);
+            });
+        }
     
 }
 
