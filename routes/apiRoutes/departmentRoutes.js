d8bd9569eee995f501396aa8c95965c5b593af24
galacticnative <68270151+galacticnative@users.noreply.github.com>
2020-10-18 02:20:05 -0700

    update routers

--- routes/apiRoutes/departmentRoutes.js
+++ routes/apiRoutes/departmentRoutes.js
@@ -62,4 +62,41 @@ router.post('/department', ({ body }, res) => {
     });
 });
 
+router.put('/department/:id', (req, res) => {
+    const errors = inputCheck(req.body, 'name');
+    if (errors) {
+      res.status(400).json({ error: errors });
+      return;
+    }
+    
+    const sql = `UPDATE department SET name = ? WHERE id = ?`;
+    const params = [req.body.name, req.params.id];
+    // use ES5 function, not arrow to use this 
+    db.run(sql, params, function(err, data) {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+  
+      res.json({
+        message: 'success',
+        data: req.body,
+        changes: this.changes
+      });
+    });
+});
+  
+router.delete('/department/:id', (req, res) => {
+    const sql = `DELETE FROM department WHERE id = ?`;
+  
+    db.run(sql, req.params.id, function(err, result) {
+      if (err) {
+        res.status(400).json({ error: res.message });
+        return;
+      }
+  
+      res.json({ message: 'deleted', changes: this.changes });
+    });
+});
+
 module.exports = router;
