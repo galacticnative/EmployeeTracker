bccaa9125e4923ee0a50eaad3b528fffbd03b521
galacticnative <68270151+galacticnative@users.noreply.github.com>
2020-10-21 18:34:44 -0700

    update reademe

--- db/database.js
+++ db/database.js
@@ -7,7 +7,7 @@ const connection = mysql.createConnection({
   // Your MySQL username
   user: 'root',
   // Your MySQL password
-  password: 'bearsamm',
+  password: '',
   database: 'business'
 });
 
